d0e76e2ce4a5f82539805d92d92330adf9c15bee
nvminh162 <nvminh162@gmail.com>
2026-07-03 10:28:09 +0700

    remove unnecessary @Repository annotations from Permission, Role, and User repositories

--- src/main/java/com/fit/iuh/user_service/repository/PermissionRepository.java
+++ src/main/java/com/fit/iuh/user_service/repository/PermissionRepository.java
@@ -1,9 +1,7 @@
 package com.fit.iuh.user_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.fit.iuh.user_service.model.Permission;
 
-@Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {}
